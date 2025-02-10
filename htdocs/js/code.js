4752add589f2f31211d9960cd38e9633f422eb8d
kylekratt <kylekratt@gmail.com>
2025-02-10 15:25:50 -0500

    Adding login function

--- htdocs/js/code.js
+++ htdocs/js/code.js
@@ -1,5 +1,64 @@
+const urlBase = 'http://proctest.christopherjparrett.xyz/php/';
+const extension = 'php';
 
 
+
+function doLogin()
+{
+	userId = 0;
+	
+	let login = document.getElementById("loginName").value;
+	let password = document.getElementById("loginPassword").value;
+//	var hash = md5( password );
+	
+	document.getElementById("loginResult").innerHTML = "";
+
+	let tmp = {login:login,password:password};
+//	var tmp = {login:login,password:hash};
+	let jsonPayload = JSON.stringify( tmp );
+	
+	let url = urlBase + '/Login.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
+				userId = jsonObject.id;
+		
+				if( userId < 1 )
+				{		
+					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+					return;
+				}
+		
+
+				saveCookie();
+	
+				window.location.href = "contactsPage.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
+}
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "userId=" + userId + ";expires=" + date.toGMTString();
+}
 
 function CheckSamePass()
 {
